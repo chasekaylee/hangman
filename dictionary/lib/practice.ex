72fde703a0aabb2c30e66edd7ebe5a7bcbd970c7
chasekaylee <chaseklee990@gmail.com>
2020-02-28 15:16:51 -0800

    first commit

---
+++ dictionary/lib/practice.ex
@@ -0,0 +1,19 @@
+defmodule PatternMatch do
+  @doc """
+  function that takes a two-element tuple as a parameter, and
+  uses pattern matching to return a two element tuple with the values
+  swapped (so pass it `{2,5}`  and you'll get `{5,2}` back).
+  """
+
+  def swap({a, b}), do: {b, a}
+
+  @doc """
+  a function that takes two parameters. It should return `true`
+  if the parameters are the same, `false` others. You aren't allowed
+  to use any conditional logic, so you'll have to rely on pattern
+  matching.
+  """
+
+  def equal(a, a), do: true
+  def equal(_, _), do: false
+end
---
+++ hangman/.elixir_ls/build/test/lib/dictionary/ebin/dictionary.app
@@ -0,0 +1,6 @@
+{application,dictionary,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"dictionary"},
+              {modules,['Elixir.Dictionary','Elixir.PatternMatch']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
